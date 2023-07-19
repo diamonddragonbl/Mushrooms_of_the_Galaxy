75bdff022ec8e6fa83c9dd491fe4d8472efd16c1
diamonddragonbl <119129397+diamonddragonbl@users.noreply.github.com>
2023-07-18 21:50:37 -0500

    Commit and Push

---
+++ player.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32, 64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft = pos)
+        self.direction = pygame.math.Vector2(0, 0)
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            pass
+        elif keys[pygame.K_LEFT]:
+            pass
+        else:
+            pass

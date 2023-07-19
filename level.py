416c1f4833c9c3eb4f30fc8077cd5393f53f09bc
diamonddragonbl <119129397+diamonddragonbl@users.noreply.github.com>
2023-07-19 11:46:50 -0500

    Commit and Push

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
 from player import Player
 
 class Level:
@@ -26,6 +26,21 @@ class Level:
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
 
+    def scroll_X(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 3
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -3
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 3
+
     def run(self):
 
         # level tiles
@@ -35,3 +50,4 @@ class Level:
         # player
         self.player.update()
         self.player.draw(self.display_surface)
+        self.scroll_X()

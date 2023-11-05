fc7fd725437d8eb9905d0142079b87b1c795611e
diamonddragonbl <119129397+diamonddragonbl@users.noreply.github.com>
2023-11-05 16:03:12 -0600

    Commit and Push

--- settings.py
+++ settings.py
@@ -2,17 +2,17 @@
 # "P" is player
 
 level_map = [
-    '                            ',
-    '                            ',
-    '                            ',
-    ' XX    XXX            XX    ',
-    ' XX P                       ',
-    ' XXXX        XX          XX ',
-    ' XXXX       XX              ',
-    ' XX    X  XXXX    XX  XX    ',
-    '       X  XXXX    XX  XXX   ',
-    '    XXXX  XXXXXX  XX  XXXX  ',
-    'XXXXXXXX  XXXXXX  XX  XXXX  ']
+    ' XXXXXXXXXXXXXXXXXXXXXXXXXX  ',
+    '                             ',
+    '                             ',
+    ' XX    XXX            XX     ',
+    ' XX P                        ',
+    ' XXXX        XX          XXX ',
+    ' XXXX       XX               ',
+    ' XX    X  XXXX    XX  XX     ',
+    '       X  XXXX    XX  XXX    ',
+    '    XXXX  XXXXXX  XX  XXXX   ',
+    'XXXXXXXX  XXXXXX  XX  XXXX   ']
 
 tile_size = 64
 screen_width = 1200
